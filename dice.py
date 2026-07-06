68441e03d53d12b4cca43412d49460ab8b9a9058
jhtet <thoonthadarhtet@gmail.com>
2026-07-06 13:32:59 -0400

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,10 @@
+##import random
+##roll_dice=random.choices([9,293,10923,4892834,123098])
+##print(roll_dice)
+##
+##roll_dice=random.choices([9,293,10923,4892834,123098],k=10)
+##print(roll_dice)
+
+import random
+randlist=random.sample(range(13,3345),12)
+print(randlist)
